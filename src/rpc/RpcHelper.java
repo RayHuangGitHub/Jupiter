dd8bffb98f9c6cf6ebe74914d316773c59d7a620
RayHuangGitHub <dirkhuang88@gmail.com>
2018-10-01 10:21:27 -0400

    First commit.

---
+++ src/rpc/RpcHelper.java
@@ -0,0 +1,68 @@
+package rpc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import entity.Item;
+
+public class RpcHelper {
+
+	// write a JSONArray to http response
+	public static void writeJsonArray(HttpServletResponse response, JSONArray array) throws IOException {
+		response.setContentType("application/json");
+		response.setHeader("Access-Control-Allow-Origin", "*"); // front-end
+		
+		PrintWriter out = response.getWriter();
+		out.print(array);
+		out.close();
+	}
+	
+	// write a JSONObject to http response
+	public static void writeJsonObject(HttpServletResponse response, JSONObject obj) throws IOException {
+		response.setContentType("application/json");
+		response.setHeader("Access-Control-Allow-Origin", "*"); // front-end
+		
+		PrintWriter out = response.getWriter();
+		out.print(obj);
+		out.close();
+	}
+	
+	// Parses a JSONObject from http request.
+	public static JSONObject readJSONObject(HttpServletRequest request) {
+		StringBuilder sb = new StringBuilder();
+		try (BufferedReader reader = request.getReader()) {
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line);
+			}
+			return new JSONObject(sb.toString());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return new JSONObject();
+	}
+	
+	// Converts a list of Item objects to JSONArray.
+	public static JSONArray getJSONArray(List<Item> items) {
+		JSONArray result = new JSONArray();
+		try {
+	      for (Item item : items) {
+	        result.put(item.toJSONObject());
+	      }
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    }
+		return result;
+    }
+
+}
